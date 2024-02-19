3bb16f952d4e3ee7bdea6f31499f5e43f4a51e3f
Yojhan Garcia <ygarcia-2022194@kinal.edu.gt>
2024-02-18 19:06:37 -0600

    Se hicieron las validaiciones de roles

---
+++ middlewares/validar-roles.js
@@ -0,0 +1,42 @@
+const Alumno = require('../models/alumno');
+
+const teacherRole = (req, res, next) =>{
+    if(!req.maestro && !req.alumno){
+        return res.status(500).json({
+            msg: "Se desea validar un usuario sin validar token primero"
+        });
+    }
+    const user = req.maestro || req.alumno;
+
+    if(user && user.role !== "TEACHER_ROLE"){
+        return res.status(401).json({
+            msg: `${user.nombre} no es un Maestro, no tines acceso a esto`
+        });
+    };
+    next
+}
+
+const tieneRolAutorizado = (...roles)=>{
+    return(req, res, next)=>{
+        if(!req.maestro && !req.alumno){
+            return res.status(500).json({
+                msg: "Se desea validar un usuario sin validar token primero"
+            });
+        };
+
+        const user = req.maestro || req.alumno;
+
+        if(user && !roles.includes(user.role)){
+            return res.status(401).json({
+                msg: `El servicio requiere uno de los siguientes roles autorizados: ${roles.join(",")}`
+            });
+        }
+        next();
+    }
+
+}
+
+module.exports = {
+    teacherRole,
+    tieneRolAutorizado
+}
